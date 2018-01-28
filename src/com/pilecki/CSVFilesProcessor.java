280797a363a858182f26cb3b006468e647163fbd
sbmaniac <sbmaniac@gmail.com>
2018-01-28 11:48:07 +0100

    Init

---
+++ src/com/pilecki/CSVFilesProcessor.java
@@ -0,0 +1,19 @@
+package com.pilecki;
+
+public class CSVFilesProcessor {
+
+    public static void main(String[] args) {
+
+       //Load file
+
+       //transform
+        //  1. Separator from ; to |
+        // quote from " to '
+        //escape char stays the same \
+
+
+
+       //save file
+
+    }
+}
